12df7c94a8020b08acfd1bb4d7c227e9d15fd497
bnski171 <125590298+bnski171@users.noreply.github.com>
2024-02-19 14:00:02 +0300

    66

--- app/src/main/java/com/raywenderlich/android/lab1/screens/ButtonsScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/ButtonsScreen.kt
@@ -28,15 +28,54 @@ fun ExploreButtonsScreen() {
 
 @Composable
 fun MyButton() {
-    //TODO add your code here
+   
+        Button(
+            onClick = (},
+    colors = Button Defaults.button Colors(background Color = color Resource(id
+    = R.color.blue)),
+    border = Border Stroke(
+            1.dp,
+    color = color Resource(id = R.color.black)
+    { { this: Row Scope
+        Text(
+            text = string Resource(id = R.string.set_button),
+            color = Color.White
 }
 
 @Composable
 fun MyRadioGroup() {
-    //TODO add your code here
+
+        val radio Buttons = list 0f(0, 1, 2) // 1
+        val selected Button = remember { mutable State 0f(radio Buttons.first()) }
+// 2
+        Column { this: Column Scope
+            radio Buttons.forEach { index -> //
+                3
+                val is Selected = index == selected Button.value
+                val colors = Radio Button Defaults.colors( // 4
+                    selected Color
+                            =
+                            color Resource(id
+                    =
+                    R.color.purple_500),
+                    unselected Color = color Resource(id = R.color.black),
+                    disabled Color = Color.Lightgray
+                            Radio Button( // 5
+                            colors = colors,
+                    selected = isSelected,
+                    onClick = { selected Button.value =
+                        index
+                    } // 6
 }
 
 @Composable
 fun MyFloatingActionButton() {
-    //TODO add your code here
+
+        FloatingActionButton(
+            onClick = (},
+    background Color = color Resource(id = R.color.purple_700),
+    content Color = Color.White,
+    content =
+        {
+            Icon(Icons.Filled.Favorite, content Description = "Test FAB")
 }
